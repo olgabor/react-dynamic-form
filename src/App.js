a06d8f983f6b078c2d7841e4ad6e3056f57041a8
Olb2 <olgaborysovska@gmail.com>
2021-04-29 23:26:40 -0400

    adds dynamic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from "react";
+import { data } from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+ 
+  const [inputFields, setInputFields] = useState(data); 
+  const [inputFiled, setValue] = useState({first_name: '',
+                                           last_name:'',
+                                           email:'',
+                                           phone_number:'', 
+                                           job_title:'',
+                                           date_of_birth:'',
+                                           parental_consent:''})
+
+  const handleSubmit = event => { 
+    event.preventDefault()
+    console.log(inputFiled);
+  }
+
+  const handleInputChange = (index, event) => {
+      setValue({...inputFiled, [event.target.name]: event.target.value.trim()})
+    }
+
+  return(
+  <> 
+    <h1> Dynamic Form </h1>
+    <form onSubmit={handleSubmit}>
+      <div className="">
+      {inputFields.map((inputField, index) => (
+        <Fragment key={index}>
+        <div className="">
+                <label>{inputField.human_label}</label>
+                <input
+                  onChange={event => handleInputChange(index, event)}
+                  type={inputField.type}
+                  className="form-control"
+                  id={inputField.name}
+                  name={inputField.name}
+                />
+              </div>
+            </Fragment>
+          ))}
+          <div className="">
+          <input type="submit" name="" id="submit"/> 
+          </div>
+       </div>
+       <br/>
+    </form>
+    </> 
+  )
 }
 
 export default App;
